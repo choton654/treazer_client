373fefbe8d9cb7f1bf74f6075f5621c521837bdf
choton654 <choton654@gmail.com>
2021-02-28 23:22:10 +0530

    initializing app

---
+++ build/precache-manifest.5819ada8a46559dade60a69029924a8d.js
@@ -0,0 +1,214 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b06d2213135507e9d1ba76626ec5a74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc08d23e5c68b201a8e5",
+    "url": "/static/js/2.7f2e3215.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7f2e3215.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b2a22db0df3e6e05002",
+    "url": "/static/js/main.bda26767.chunk.js"
+  },
+  {
+    "revision": "9d56b0ea2705241215be",
+    "url": "/static/js/runtime-main.cf8c75f0.js"
+  },
+  {
+    "revision": "2db113dd0f1492d7f98521db924f9479",
+    "url": "/static/media/avatar-1.2db113dd.jpg"
+  },
+  {
+    "revision": "7087d1200629188b9c162f02776c3608",
+    "url": "/static/media/avatar-2.7087d120.jpg"
+  },
+  {
+    "revision": "6f2bab193b29900a280514754925b38e",
+    "url": "/static/media/avatar-3.6f2bab19.jpg"
+  },
+  {
+    "revision": "bc841a07000852731a734fd46efbce97",
+    "url": "/static/media/avatar-4.bc841a07.jpg"
+  },
+  {
+    "revision": "d7fd9e0b952d5f9b9adff6ec29a8b20d",
+    "url": "/static/media/avatar-5.d7fd9e0b.jpg"
+  },
+  {
+    "revision": "df1164e02e97806424d6159b48eb0309",
+    "url": "/static/media/baked-fries.df1164e0.jpg"
+  },
+  {
+    "revision": "43d062550612eaa49038a9286ae5814c",
+    "url": "/static/media/burger-restaurant-2.43d06255.jpg"
+  },
+  {
+    "revision": "ed364d0718e59ee5583c4d5587afcdbf",
+    "url": "/static/media/burger-restaurant.ed364d07.jpg"
+  },
+  {
+    "revision": "b826d84efc394c891e919f6d3494207b",
+    "url": "/static/media/chicago-hot-dog.b826d84e.jpg"
+  },
+  {
+    "revision": "e6bc3e8362dad243b0dbcdbf09e1cd0c",
+    "url": "/static/media/crispy-chicken-burger.e6bc3e83.jpg"
+  },
+  {
+    "revision": "dc8435a3ff797b7f51c3523aed821d03",
+    "url": "/static/media/cutlery.dc8435a3.png"
+  },
+  {
+    "revision": "19fac450af106b5d24f68f4dd9ced3e1",
+    "url": "/static/media/donut.19fac450.png"
+  },
+  {
+    "revision": "12be6f741a94158388b97fbb78f29bf7",
+    "url": "/static/media/drink.12be6f74.png"
+  },
+  {
+    "revision": "e690edd6434e1ed3175aacc429b98a66",
+    "url": "/static/media/fire.e690edd6.png"
+  },
+  {
+    "revision": "e1b8e05fe3d59e56706b1af20e1ef792",
+    "url": "/static/media/fries-restaurant.e1b8e05f.jpg"
+  },
+  {
+    "revision": "4b35dec44587282ef4a0f7a97e4a0eda",
+    "url": "/static/media/fries.4b35dec4.png"
+  },
+  {
+    "revision": "fdb953e747052b088eda7e2d8bc116b9",
+    "url": "/static/media/hamburger.fdb953e7.png"
+  },
+  {
+    "revision": "b2979841d2a539d4949ddf23f5d85032",
+    "url": "/static/media/hawaiian-pizza.b2979841.jpg"
+  },
+  {
+    "revision": "3d68efc87d21dd03eb71b17f82b8805a",
+    "url": "/static/media/honey-mustard-chicken-burger.3d68efc8.jpg"
+  },
+  {
+    "revision": "6d6ffec9206e750b5fb21282492af08a",
+    "url": "/static/media/hot-dog-restaurant.6d6ffec9.jpg"
+  },
+  {
+    "revision": "9c51807bde69cfc20b02a273fd6b22bd",
+    "url": "/static/media/hotdog.9c51807b.png"
+  },
+  {
+    "revision": "7b5d40ce7ffd406fa3cc9474f8ca44fa",
+    "url": "/static/media/japanese-restaurant.7b5d40ce.jpg"
+  },
+  {
+    "revision": "377a2b8aa35dfb5f808b87bb3a98cc92",
+    "url": "/static/media/kek-lapis-shop.377a2b8a.jpg"
+  },
+  {
+    "revision": "ea171e18012effca60098446de9580fd",
+    "url": "/static/media/kek-lapis.ea171e18.jpg"
+  },
+  {
+    "revision": "1f2c976b908026f5dec8522bdda5fa74",
+    "url": "/static/media/kolo-mee.1f2c976b.jpg"
+  },
+  {
+    "revision": "53263c3a923ed245e3f67b9dd52283fb",
+    "url": "/static/media/like.53263c3a.png"
+  },
+  {
+    "revision": "d724f141261268d0f2ea646ebe7a696a",
+    "url": "/static/media/location.d724f141.png"
+  },
+  {
+    "revision": "6fbba6a580534be849d9a8b3b37c922e",
+    "url": "/static/media/mastercard.6fbba6a5.png"
+  },
+  {
+    "revision": "49beb757e64a952fde3e80a210dbf2dc",
+    "url": "/static/media/nasi-briyani-mutton.49beb757.jpg"
+  },
+  {
+    "revision": "311e79c843e941e36050cc14cbb486bd",
+    "url": "/static/media/nasi-lemak.311e79c8.jpg"
+  },
+  {
+    "revision": "3b382d3af74e46cc962656121e732b59",
+    "url": "/static/media/nearby.3b382d3a.png"
+  },
+  {
+    "revision": "99df18bd4db233e9e169b0ae45cb97be",
+    "url": "/static/media/noodle-shop.99df18bd.jpg"
+  },
+  {
+    "revision": "74531344471b8271d8ff987d2a215abc",
+    "url": "/static/media/noodle.74531344.png"
+  },
+  {
+    "revision": "62331cf8c9394b59c9d4dcae8ad9ed3c",
+    "url": "/static/media/pin.62331cf8.png"
+  },
+  {
+    "revision": "e692e1cfcb25f5ac0fddf21e1403072c",
+    "url": "/static/media/pizza-restaurant.e692e1cf.jpg"
+  },
+  {
+    "revision": "6886695afb9bc078c96bcb218a72a419",
+    "url": "/static/media/pizza.6886695a.jpg"
+  },
+  {
+    "revision": "a3570c72dbfa426314567e68be888750",
+    "url": "/static/media/pizza.a3570c72.png"
+  },
+  {
+    "revision": "df87fb33bda0c3ece9cfdde8b62d223b",
+    "url": "/static/media/red-pin.df87fb33.png"
+  },
+  {
+    "revision": "f19d662ed1506286784b11ffb6fdee2c",
+    "url": "/static/media/rice-bowl.f19d662e.png"
+  },
+  {
+    "revision": "7f91fbf07f1e51f9c58fb8bb1e9890fc",
+    "url": "/static/media/salad.7f91fbf0.png"
+  },
+  {
+    "revision": "57bbdb688cdd8751b82d0667731c3c24",
+    "url": "/static/media/sarawak-laksa.57bbdb68.jpg"
+  },
+  {
+    "revision": "dd770b4336c45d2a9c8c58e8fbd7c322",
+    "url": "/static/media/search.dd770b43.png"
+  },
+  {
+    "revision": "45dd54703cf1537d439bc358c680762c",
+    "url": "/static/media/star.45dd5470.png"
+  },
+  {
+    "revision": "258d48d717e29b082f567386111e3298",
+    "url": "/static/media/sushi.258d48d7.jpg"
+  },
+  {
+    "revision": "e4b1cb4fd5ef3f171dcd5d0c0b64612a",
+    "url": "/static/media/sushi.e4b1cb4f.png"
+  },
+  {
+    "revision": "ce4f0acd4ceb5e5031bf85027651a183",
+    "url": "/static/media/teh-c-peng.ce4f0acd.jpg"
+  },
+  {
+    "revision": "20beb9a706cf492af9257982b0899eac",
+    "url": "/static/media/tomato-pasta.20beb9a7.jpg"
+  },
+  {
+    "revision": "69bb0ec11e240e166a968e0635462a2c",
+    "url": "/static/media/user.69bb0ec1.png"
+  }
+]);
